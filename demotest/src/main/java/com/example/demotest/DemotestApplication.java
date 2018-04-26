9a417a61dd40e2a7069f43d664502e0fda61ea43
gfzhujie <gfzhujie@oa.gf.com.cn>
2018-04-26 13:05:32 +0800

    update

--- demotest/src/main/java/com/example/demotest/DemotestApplication.java
+++ demotest/src/main/java/com/example/demotest/DemotestApplication.java
@@ -4,13 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @SpringBootApplication
 public class DemotestApplication {
 
+	protected static Logger logger=LoggerFactory.getLogger(DemotestApplication.class);
+
 	@RequestMapping("/test")
 	public String index(){
+		logger.debug("访问hello");
 		return "Hello Spring Boot";
 	}
 

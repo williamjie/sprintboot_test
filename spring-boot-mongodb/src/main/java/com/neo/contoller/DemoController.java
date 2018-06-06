22e6b994c0fe02fc0127dd5447dbfd76dc2c0b11
jiez216 <jiez216@163.com>
2018-06-06 13:21:44 +0800

    update

--- spring-boot-mongodb/src/main/java/com/neo/contoller/DemoController.java
+++ spring-boot-mongodb/src/main/java/com/neo/contoller/DemoController.java
@@ -13,6 +13,7 @@ import com.neo.entity.UserEntity;
 import com.neo.dao.UserDao;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DemoController {
@@ -27,13 +28,30 @@ public class DemoController {
     public String getUser(@PathVariable("id") Long id,@PathVariable("name") String name,@PathVariable("pwd") String pwd ) {
         UserEntity user = new UserEntity();
         //Long xx = Long.parseLong();
+        System.out.print("GetMongoUserName------------id:"+id+" name:"+name+" pwd:"+pwd);
+
         user.setId(id);
         user.setUserName(name);
         user.setPassWord(pwd);
         userDao.saveUser(user);
-        return "hello world";
+        return user.toString();
+        //return "hello world";
         //return service.getUser(id);
-    }
+    } //http://localhost:9090/GetMongoUserName/123/名字/密码
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String sayHello(@RequestParam("id") Long id,@RequestParam("name") String name,@RequestParam("pwd") String pwd){
+        //return "id:"+id;
+        System.out.print("hello------------id:"+id+" name:"+name+" pwd:"+pwd);
+
+        UserEntity user = new UserEntity();
+        user.setId(id);
+        user.setUserName(name);
+        user.setPassWord(pwd);
+        userDao.saveUser(user);
+        return user.toString();
+    } //localhost:9090/hello?id=1000
+
     /*
     @RequestMapping(value = "/test",method = RequestMethod.POST)
     public void demoTest(){

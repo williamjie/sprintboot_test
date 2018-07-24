61601371c033bfb7eabaf3c622d067ee8088f37d
williamjie <jiez216@163.com>
2018-07-24 16:10:03 +0800

    update

---
+++ jpa4test/src/main/java/com/example/jpa4test/Jpa4testApplication.java
@@ -0,0 +1,12 @@
+package com.example.jpa4test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa4testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa4testApplication.class, args);
+	}
+}

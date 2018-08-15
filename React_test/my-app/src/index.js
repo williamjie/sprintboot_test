278aceff8a5afb75b47026d7d87754ba03124ff4
williamjie <jiez216@163.com>
2018-08-15 17:37:57 +0800

    update

--- React_test/my-app/src/index.js
+++ React_test/my-app/src/index.js
@@ -4,5 +4,29 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+class LikeButton extends Component {
+    constructor () {
+      super()
+      this.state = { isLiked: false }
+    }
+
+    onClick () {
+      this.setState({
+        isLiked: !this.state.isLiked
+      })
+    }
+
+    render () {
+      return `
+        <button class='like-btn'>
+          <span class='like-text'>${this.state.isLiked ? '取消' : '点赞'}</span>
+          <span>👍</span>
+        </button>
+      `
+    }
+  }
+
+  mount(new LikeButton(), wrapper)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
